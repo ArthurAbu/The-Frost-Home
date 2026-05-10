5889d7ea59827be85707a1c00728502d7454c3de
ArthurAbu <arturabusahmin520@gmail.com>
2026-05-10 18:57:45 +0500

    Кнопки главного меню

---
+++ MainMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Управляет кнопками главного меню.
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private string gameplaySceneName = "MainMap"; 
+
+    public void PlayGame()
+    {
+        // Загружаем сцену игры
+        SceneManager.LoadScene(gameplaySceneName);
+    }
+
+    public void ResetProgress()
+    {
+        PlayerPrefs.SetInt("CurrentDay", 0);
+        PlayerPrefs.Save();
+    }
+
+    public void QuitGame()
+    {
+        // Разблокируем курсор перед выходом
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        // Выход из игры
+        Application.Quit();
+    }
+}
